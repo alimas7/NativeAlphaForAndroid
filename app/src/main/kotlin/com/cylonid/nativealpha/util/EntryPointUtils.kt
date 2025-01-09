ebfa27b6326e7589a71be03a0e41d2c1fe07faae
alimas7 <alimas711@yahoo.com>
2025-01-08 20:03:41 -0800

    Update EntryPointUtils.kt

--- app/src/main/kotlin/com/cylonid/nativealpha/util/EntryPointUtils.kt
+++ app/src/main/kotlin/com/cylonid/nativealpha/util/EntryPointUtils.kt
@@ -9,7 +9,8 @@ import com.cylonid.nativealpha.activities.NewsActivity
 object EntryPointUtils {
     @JvmStatic
     fun entryPointReached(a: Activity) {
-        if (kotlin.math.abs(DataManager.getLastShownUpdate() - BuildConfig.VERSION_CODE) > 10 ) {
+        val dif: Int = DataManager.getLastShownUpdate() - BuildConfig.VERSION_CODE
+        if (kotlin.math.abs(dif) > 10 ) {
             a.startActivity(Intent(a, NewsActivity::class.java))
         }
         DataManager.getInstance().loadAppData()
